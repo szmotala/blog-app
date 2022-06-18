dc486fa1883e66f734b48b303979b65e0d8b72ad
Szymon Motała <szymon.motala95@gmail.com>
2022-06-18 21:44:35 +0200

    Blog application add to repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
+// Setup your firebase api
+// and paste it here:
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: 'AIzaSyCFl7oRVXvsqgIWRuTTi4KMqH_jfRjRa0U',
-    authDomain: 'karo-blog-app.firebaseapp.com',
-    projectId: 'karo-blog-app',
-    storageBucket: 'karo-blog-app.appspot.com',
-    messagingSenderId: '210298639821',
-    appId: '1:210298639821:web:e374d0502b3d3bb379e084',
-    measurementId: 'G-63LFPGDXKM',
+    apiKey: 'your_api',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+// Setup your firebase api
+// and paste it here:
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCFl7oRVXvsqgIWRuTTi4KMqH_jfRjRa0U',
-    authDomain: 'karo-blog-app.firebaseapp.com',
-    projectId: 'karo-blog-app',
-    storageBucket: 'karo-blog-app.appspot.com',
-    messagingSenderId: '210298639821',
-    appId: '1:210298639821:web:e374d0502b3d3bb379e084',
-    measurementId: 'G-63LFPGDXKM',
+    apiKey: 'your_api',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
